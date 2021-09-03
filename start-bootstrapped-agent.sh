40224b67e0415a825619b7dbc1b49d6632df832f
Adam Neumann <noizwaves@users.noreply.github.com>
2021-09-03 17:42:04 -0600

    Add pre-exit hook and turn off debug logging

--- start-bootstrapped-agent.sh
+++ start-bootstrapped-agent.sh
@@ -12,7 +12,9 @@ docker run \
   -e BUILDKITE_AGENT_TOKEN \
   -e BUILDKITE_AGENT_NAME="bootstrapped-agent" \
   -e BUILDKITE_AGENT_TAGS="queue=bootstrap-issue" \
-  -e BUILDKITE_AGENT_DEBUG="true" \
+  -e BUILDKITE_AGENT_DEBUG="false" \
   -e BUILDKITE_BOOTSTRAP_SCRIPT_PATH="/etc/buildkite-agent/bootstrap.sh" \
   -v "${SCRIPT_DIR}/bootstrap.sh:/etc/buildkite-agent/bootstrap.sh:ro" \
+  -e BUILDKITE_HOOKS_PATH="/etc/buildkite-agent/hooks/" \
+  -v "${SCRIPT_DIR}/pre-exit.sh:/etc/buildkite-agent/hooks/pre-exit:ro" \
   buildkite/agent:3.27
--- start-default-agent.sh
+++ start-default-agent.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Usage: BUILDKITE_AGENT_TOKEN=<xyz> ./start-default-agent.sh
 
+# https://stackoverflow.com/a/246128
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 docker rm buildkite-bootstrap-issue
 
 docker run \
@@ -9,5 +12,7 @@ docker run \
   -e BUILDKITE_AGENT_TOKEN \
   -e BUILDKITE_AGENT_NAME="default-agent" \
   -e BUILDKITE_AGENT_TAGS="queue=bootstrap-issue" \
-  -e BUILDKITE_AGENT_DEBUG="true" \
+  -e BUILDKITE_AGENT_DEBUG="false" \
+  -e BUILDKITE_HOOKS_PATH="/etc/buildkite-agent/hooks/" \
+  -v "${SCRIPT_DIR}/pre-exit.sh:/etc/buildkite-agent/hooks/pre-exit:ro" \
   buildkite/agent:3.27
