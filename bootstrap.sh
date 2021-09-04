a97e9de76f10cf66b0f61612fe7de1c5ca499421
Adam Neumann <noizwaves@users.noreply.github.com>
2021-09-03 19:34:48 -0600

    Correctly handle signals and hooks, still need exit code propagation

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
-buildkite-agent bootstrap
+
+# Forward SIGTERM to child
+# https://unix.stackexchange.com/questions/146756/forward-sigterm-to-child-in-bash
+
+_term() {
+  # forward TERM signal to child
+  kill -TERM "$child"
+
+  # and wait for child to terminate
+  wait "$child"
+  TERM_EXIT_CODE=$?
+
+  # exit bootstrap.sh with the child's exit code
+  exit $TERM_EXIT_CODE
+}
+
+trap _term SIGTERM
+
+# Run the tee in a subshell to we can background a process using redirects
+# Start commands in new process groups to skip termination by Buildkite Agent
+setsid buildkite-agent bootstrap > >(setsid tee /tmp/buildkite-job-${BUILDKITE_JOB_ID}.log) &
+child=$!
+
+wait "$child"
+NON_TERM_EXIT_CODE=$?
+
+# exit bootstrap script with the child's exit code
+exit $NON_TERM_EXIT_CODE
