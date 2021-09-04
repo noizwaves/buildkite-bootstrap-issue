f7a4fc20c1a78c739f74603a3f934559427eedf1
Adam Neumann <noizwaves@users.noreply.github.com>
2021-09-03 20:26:21 -0600

    Refactor function name

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
-# Forward SIGTERM to child
+# Forward SIGTERM to PID
 # https://unix.stackexchange.com/questions/146756/forward-sigterm-to-child-in-bash
 
-_term() {
-  # forward TERM signal to child
-  kill -TERM "$child"
+_forward_signal() {
+  SIGNAL=$1
 
-  # and wait for child to terminate
-  wait "$child"
+  # forward $SIGNAL to child
+  kill -$SIGNAL "$PID"
+
+  # and wait for PID to terminate
+  wait "$PID"
   TERM_EXIT_CODE=$?
 
   # delete job logs after bootstrap is complete
   rm -rf /tmp/buildkite-job-${BUILDKITE_JOB_ID}.log
 
-  # exit bootstrap.sh with the child's exit code
+  # exit bootstrap.sh with the PID's exit code
   exit $TERM_EXIT_CODE
 }
 
-trap _term SIGTERM
+trap "_forward_signal TERM" SIGTERM
 
 # Run the tee in a subshell to we can background a process using redirects
 # Start commands in new process groups to skip termination by Buildkite Agent
 setsid buildkite-agent bootstrap > >(setsid tee /tmp/buildkite-job-${BUILDKITE_JOB_ID}.log) &
-child=$!
+PID=$!
 
-wait "$child"
+wait "$PID"
 NON_TERM_EXIT_CODE=$?
 
 # delete job logs after bootstrap is complete
 rm -rf /tmp/buildkite-job-${BUILDKITE_JOB_ID}.log
 
-# exit bootstrap script with the child's exit code
+# exit bootstrap script with the PID's exit code
 exit $NON_TERM_EXIT_CODE
